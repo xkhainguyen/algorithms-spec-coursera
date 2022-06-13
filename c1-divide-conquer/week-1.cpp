8390f71ec2860e15b448a29598f03c3b37a7ec87
Nguyen Xuan Khai <khai.nx173970@sis.hust.edu.vn>
2022-06-13 17:46:11 +0700

    add cpp

--- c1-divide-conquer/week-1.cpp
+++ c1-divide-conquer/week-1.cpp
@@ -1,12 +1,14 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <math.h>       /* ceil */
+#include <cmath>
 
 using namespace std;
 
 class Karatsuba
 {
-    //Karatsuba's alogrithm on integer multiplication
+    /* Karatsuba's alogrithm on integer multiplication */
     public:
 
     vector<string> pad_zero(string num1, string num2)
@@ -22,22 +24,47 @@ class Karatsuba
         return num;
     }
 
-    // vector<string> mul(const string num1, const string num2)
-    // {
-    //     vector<string> num;
-    //     int n;
-    //     num = pad_zero(num1, num2);
-    //     n = num[1].length();
+    string mul(const string num1, const string num2)
+    {
+        vector<string> num = pad_zero(num1, num2);
+        int n = num[1].length();
+        if (n > 1)
+        {
+            int hdigit = ceil(n/2.);
 
-    // } 
+            string a = num[0].substr(0, hdigit);
+            string b = num[0].substr(hdigit, n - hdigit);
+            string c = num[1].substr(0, hdigit);
+            string d = num[1].substr(hdigit, n - hdigit);
+
+            string ac = mul(a, c);
+            string bd = mul(b, d);
+
+            string a_b = to_string(stoi(a) + stoi(b));
+            string c_d = to_string(stoi(c) + stoi(d));
+            string sum = mul(a_b, c_d);
+            string ad_bc = to_string(stoi(sum) - stoi(ac) - stoi(bd));
+
+            int a1 = stoi(ac)*pow(10,((n - hdigit)*2));
+            int a2 = stoi(ad_bc)*pow(10,(n - hdigit));
+            int a3 = stoi(bd);
+            string res = to_string(a1 + a2 + a3);
+            // cout << res << endl;
+            return res;
+        }            
+        else
+            return to_string(stoi(num1)*stoi(num2));
+    } 
 };
 
 int main()
 {
     Karatsuba alg;
-    string num1 = {"1111"};
-    string num2 = {"23"};
+    string num1 = {"12593"};    // Only work with small numbers
+    string num2 = {"36623"};
     vector<string> num = alg.pad_zero(num1, num2);
     for (const string& c : num)
         std::cout << c << ' ';
+    cout << endl << alg.mul(num1, num2) << endl;
+    cout << stoi(num1)*stoi(num2);
 }
